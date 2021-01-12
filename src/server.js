1f75a23d2fc09a7d799aa8017f735cf4dc1bf9f8
Uria Rogel <uria_rogel@hotmail.com>
2021-01-12 23:10:31 +0100

    use dotenv

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
+require('dotenv').config()
+const https = require('https');
+const fs = require('fs');
+
+const {
+  PATH_TO_KEY,
+  PATH_TO_CERT,
+} = process.env
+
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.argv[2] || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const options = {
+  key: fs.readFileSync(PATH_TO_KEY),
+  cert: fs.readFileSync(PATH_TO_CERT)
+};
+
+https
+.createServer(options, app)
+.listen(port, () => {
+  console.log(`Example app listening at https://localhost:${port}`)
+});
