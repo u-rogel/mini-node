0f1c78b9912c926f06ed0d5a69e832c0c6ac3658
Uria Rogel <uria_rogel@hotmail.com>
2021-01-18 00:23:03 +0100

    moving to docker-compose

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,24 @@
 require('dotenv').config()
 const https = require('https');
 const fs = require('fs');
+const express = require('express')
+
+const app = express()
+const port = process.argv[2] || 3000
 
 const {
   PATH_TO_KEY,
   PATH_TO_CERT,
 } = process.env
 
-const express = require('express')
-const app = express()
-const port = process.argv[2] || 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 const options = {
   key: fs.readFileSync(PATH_TO_KEY),
   cert: fs.readFileSync(PATH_TO_CERT)
 };
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 https
 .createServer(options, app)
